8cde82506030d8d627683b9047911af870dfc07b
naripok <fernando.canteruccio@gmail.com>
2023-08-04 19:13:58 -0300

    feat: add greenlet implementation

---
+++ tests/test_greenlet_get_scopefunc.py
@@ -0,0 +1,51 @@
+import greenlet
+import pytest
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_webtest import get_scopefunc
+
+session_options = {
+    "scopefunc": get_scopefunc(
+        # We think this should be the correct implementation
+        # however, the test still fails. Why?
+        original_scopefunc=lambda: id(greenlet.getcurrent())
+    )
+}
+db = SQLAlchemy(session_options=session_options)
+
+
+class Event(db.Model):
+    id = db.Column(db.Integer(), primary_key=True)
+
+
+def create_app():
+    app = Flask(__name__)
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///:memory:"
+    db.init_app(app)
+    return app
+
+
+@pytest.fixture(autouse=True)
+def app():
+    _app = create_app()
+    with _app.test_request_context():
+        yield _app
+
+
+@pytest.fixture(name="db")
+def fixture_db():
+    db.create_all()
+    yield db
+    db.session.remove()
+    db.drop_all()
+
+
+def test_create(app, db):
+    event = Event()
+    db.session.add(event)
+    db.session.commit()
+
+    assert event in db.session
+
+    with app.app_context():
+        assert event not in db.session
